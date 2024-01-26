a81eef46ff7ca594d255351a95597d574e363b3f
Vinzenz Sinapius <Vinzenz.Sinapius@gmail.com>
2024-01-26 22:06:51 +0100

    initial unity project

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class GameManager : MonoBehaviour
+{
+
+    private static GameManager instance;
+
+    public GameObject PersonPrefab;
+
+
+    private List<Person> audience = new List<Person>();
+
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<GameManager>();
+            }
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+
+        }
+    }
+
+    void Start()
+    {
+        PopulateAudience();
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+
+    public void PopulateAudience()
+    {
+        GameObject[] chairs = GameObject.FindGameObjectsWithTag("Chair");
+
+        foreach (GameObject chair in chairs)
+        {
+            GameObject newPerson = Instantiate(PersonPrefab,chair.transform.position + new Vector3(0,0.5f,0), Quaternion.identity);
+            audience.Add(newPerson.GetComponent<Person>());
+        }
+    }
+
+    public void TellAJoke()
+    {
+        
+        foreach(Person person in audience)
+        {
+            // calculate each persons enjoyment
+
+            // Update persons behavior
+
+            // Update Score
+        }
+
+    }
+}
---
+++ Assets/Scripts/Person.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.PlayerSettings;
+
+public class Person : MonoBehaviour
+{
+    private Vector3 startPosition;
+
+
+    private Vector3 targetWobbleAmplitude;
+    private Vector3 targetWobbleFrequency;
+
+    private Vector3 currentWobbleFrequency;
+    private Vector3 currentWobbleAmplitude;
+    private Vector3 currentWobblePhase;
+
+
+    public float enjoymentValue;
+
+    public enum BehaviorState
+    {
+        Neutral,
+        Happy,
+        Booing,
+        Angry,
+        Leaving
+    }
+
+    public BehaviorState behaviorState = BehaviorState.Neutral;
+
+    void Start()
+    {
+        startPosition = transform.position + new Vector3(0, 1.5f, 0);
+        currentWobblePhase = new Vector3(Random.value, Random.value, Random.value);
+    }
+
+
+    void Update()
+    {
+        UpdateBehavior();
+        UpdateMovement();
+    }
+
+
+    private void UpdateBehavior()
+    {
+        switch (behaviorState)
+        {
+            case BehaviorState.Neutral:
+                UpdateNeutral();
+                break;
+            case BehaviorState.Happy:
+                UpdateHappy();
+                break;
+            case BehaviorState.Booing:
+                UpdateBooing();
+                break;
+            case BehaviorState.Angry:
+                UpdateAngry();
+                break;
+            case BehaviorState.Leaving:
+                UpdateLeaving();
+                break;
+
+        }
+    }
+
+    private void UpdateMovement()
+    {
+        currentWobbleAmplitude = Vector3.Lerp(currentWobbleAmplitude,targetWobbleAmplitude,Time.deltaTime * 0.1f);
+        currentWobbleFrequency = Vector3.Lerp(currentWobbleFrequency, targetWobbleFrequency, Time.deltaTime * 0.1f);
+
+
+        Vector3 pos = transform.position;
+        Vector3 wobbleOffset = new Vector3(Mathf.Sin(currentWobblePhase.x) * currentWobbleAmplitude.x,
+                                           Mathf.Sin(currentWobblePhase.y) * currentWobbleAmplitude.y,
+                                           Mathf.Sin(currentWobblePhase.z) * currentWobbleAmplitude.z);
+
+        transform.position = startPosition + wobbleOffset;
+
+        currentWobblePhase += currentWobbleFrequency * (Time.deltaTime * Mathf.PI * 2);
+    }
+
+    private void UpdateNeutral()
+    {
+        targetWobbleAmplitude = new Vector3(0.01f,0.05f,0.01f);
+        targetWobbleFrequency = new Vector3(0.5f,0.5f,0.5f);
+    }
+    private void UpdateHappy()
+    {
+        targetWobbleAmplitude = new Vector3(0.01f, 0.2f, 0.01f);
+        targetWobbleFrequency = new Vector3(0.5f, 0.8f, 0.5f);
+    }
+
+    private void UpdateBooing()
+    {
+        targetWobbleAmplitude = new Vector3(0.05f, 0.2f, 0.05f);
+        targetWobbleFrequency = new Vector3(0.8f, 1f, 0.8f);
+    }
+
+    private void UpdateAngry()
+    {
+
+    }
+
+    private void UpdateLeaving()
+    {
+
+    }
+
+
+
+}
