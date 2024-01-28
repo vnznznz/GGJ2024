8faa5180e74fdc091b6afb0b2073140e338cc396
Vinzenz Sinapius <Vinzenz.Sinapius@gmail.com>
2024-01-28 14:46:35 +0100

    Add card icons

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -33,11 +33,34 @@ public class Card : MonoBehaviour
 
     public Image image;
 
+    public NamedImage[] icons;
+
+    public Image iconMaleBoomer;
+    public Image iconFemaleBoomer;
+    public Image iconMaleMillenial;
+    public Image iconFemaleMillenial;
+    public Image iconMaleGenz;
+    public Image iconFemaleGenz;
+
+    public Sprite questionSprite;
+
+    private Dictionary<string, Sprite> iconImages = new Dictionary<string, Sprite>();
+
     private void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
         comedyActionsLoader = FindObjectOfType<ComedyActionsLoader>();
+        iconMaleBoomer = transform.Find("effectIcon1_male_boomer").GetComponent<Image>();
+        iconFemaleBoomer = transform.Find("effectIcon1_female_boomer").GetComponent<Image>();
+        iconMaleMillenial = transform.Find("effectIcon1_male_millenial").GetComponent<Image>();
+        iconFemaleMillenial = transform.Find("effectIcon1_female_millenial").GetComponent<Image>();
+        iconMaleGenz = transform.Find("effectIcon1_male_genz").GetComponent<Image>();
+        iconFemaleGenz = transform.Find("effectIcon1_female_genz").GetComponent<Image>();
 
+        foreach (var item in icons)
+        {
+            iconImages[item.name] = item.image;
+        }
     }
 
     private void Start()
@@ -45,6 +68,89 @@ public class Card : MonoBehaviour
         text.text = joke.text;
         title.text = joke.title;
         LoadTitleImage();
+
+    }
+
+    public void SetIcons()
+    {
+        int remaingTips = 3;
+        foreach (var gender in new string[] { "male", "female" })
+        {
+            foreach (var age in new string[] { "boomer", "millenial", "genz" })
+            {
+
+                var enjoyment = 0;
+                foreach (var result in joke.result)
+                {
+                    if (result.audience == age || result.audience == gender)
+                    {
+                        enjoyment += result.value;
+                    }
+                }
+
+                var modifier = "";
+                if (enjoyment > 0)
+                {
+                    modifier = "happy";
+                }
+                else if (enjoyment < 0)
+                {
+                    modifier = "unhappy";
+                }
+                Image currentIcon = iconMaleBoomer;
+                var bla = gender + "_" + age;
+                if (bla == "female_boomer")
+                {
+                    currentIcon = iconFemaleBoomer;
+                }
+                else if (bla == "male_millenial")
+                {
+                    currentIcon = iconMaleMillenial;
+                }
+                else if (bla == "female_millenial")
+                {
+                    currentIcon = iconFemaleMillenial;
+                }
+                else if (bla == "male_genz")
+                {
+                    currentIcon = iconMaleGenz;
+                }
+                else if (bla == "female_genz")
+                {
+                    currentIcon = iconFemaleGenz;
+                }
+
+                var audience = gender + "_" + age + "_" + modifier;
+                if (modifier != "")
+                {
+
+
+                    if (UnityEngine.Random.Range(0f, 1f) > 0.5f)
+                    {
+                        currentIcon.color = Color.white;
+                        currentIcon.sprite = iconImages[audience];
+                        remaingTips -= 1;
+                    }
+                    else
+                    {
+
+                        currentIcon.sprite = null;
+                    }
+                }
+                else
+                {
+                    currentIcon.sprite = null;
+
+                }
+
+                if (remaingTips <= 0)
+                {
+                    return;
+                }
+
+            }
+        }
+
     }
 
     private void Update()
